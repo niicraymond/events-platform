4cb3790ed764c65fcee7a7ab7becce9aea0c6840
Nicole Raymond <nicoleraymond@hotmail.com>
2025-10-15 16:31:41 +0100

    adds event details page

--- frontend/vite-project/src/pages/EventDetails.jsx
+++ frontend/vite-project/src/pages/EventDetails.jsx
@@ -1,6 +1,22 @@
+import { useParams } from "react-router-dom";
+import { mockEvents } from "../data";
+
 function EventDetails() {
-    return (
-        <p>This is Event Details</p>
-    )
+  const { id } = useParams();
+  const event = mockEvents.find((e) => e.id === Number(id));
+
+  if (!event) return <h2>Event not found.</h2>;
+
+  return (
+    <div>
+      <h1>{event.title}</h1>
+      <p>
+        {event.date} • {event.location}
+      </p>
+      <p>{event.description}</p>
+      <img src={event.image} alt={event.title} />
+      <button>{event.isPaid ? "Pay Now" : "Sign Up"}</button>
+    </div>
+  );
 }
 export default EventDetails;
